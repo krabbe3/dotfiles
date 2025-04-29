196c27fa1954936acfbb35bd4bc97b3923a8e8e7
Lorenz <lorenz.mohr@tu-ilmenau.de>
2025-04-29 16:20:18 +0200

    oil with autocommand preview window + toggle

--- nvim/.config/nvim/lua/plugins/oil.lua
+++ nvim/.config/nvim/lua/plugins/oil.lua
@@ -40,10 +40,24 @@ return {
                     preview_method = "fast_scratch", 
                 },
 
+                vim.api.nvim_create_autocmd("User", {
+                    pattern = "OilEnter",
+                    callback = vim.schedule_wrap(function(args)
+                        local oil = require("oil")
+                        if vim.api.nvim_get_current_buf() == args.data.buf and oil.get_cursor_entry() then
+                            oil.open_preview({
+                                vertical = true,
+                                split = "belowright",
+                            })
+                        end
+                    end),
+                }),                  
+
                 -- Keymaps
                 keymaps = {
                     ["g?"] = { "actions.show_help", mode = "n" , desc = "Show default keymaps"},
-                    ["<leader>p"] = { "actions.preview", opts = { horizontal = true, split = "botright"}, desc = "Show file preview" },
+                    ["<leader>p"] = { "actions.preview", opts = { vertical = true, split = "botright"}, desc = "Show file preview" },
+                    -- go to home dir
                     ["~"] = "<cmd>edit $HOME<CR>",
                     ["<leader>jk"] = { "actions.close", desc = "Close oil"},
                     ["<leader>h"] = { "actions.parent", desc = "Go parent dir"},
