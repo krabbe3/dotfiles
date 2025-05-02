fcc3b6cb3ca8ebeef8b2dc1f1e8cee0413a7b3cf
Lorenz <lorenz.mohr@tu-ilmenau.de>
2025-05-02 13:00:35 +0200

    "fugitive addon"

---
+++ nvim/.config/nvim/lua/plugins/fugitive.lua
@@ -0,0 +1,8 @@
+return{
+    "tpope/vim-fugitive",
+    keys = {
+        { "<leader>gs", "<cmd>Git<CR>", desc = "Git Status (Fugitive)"},
+        { "<leader>gb", "<cmd>Git blame<CR>", desc = "Git Blame (Fugitive)"},
+        { "<leader>gc", "<cmd>Git commit<CR>", desc = "Git Commit (Fugitive)"},
+    }
+}
