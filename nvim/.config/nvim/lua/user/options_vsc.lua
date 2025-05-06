f601c431443b1836838e01f49b4670ad35656df0
Lorenz <lorenz.mohr@tu-ilmenau.de>
2025-05-06 14:26:25 +0200

    removed old vscode files

--- nvim/.config/nvim/lua/user/options_vsc.lua
+++
@@ -1,9 +0,0 @@
--- configuration file for user defined neovim options
-local opt = vim.opt
-
--- basic options
-opt.shiftwidth = 4                      -- number of spaces for an indentation
-opt.tabstop = 4	                    -- 2 spaces for a tab
-opt.softtabstop = 0		          -- softabs
-opt.expandtab = true                    -- convert tabs to spaces
---opt.clipboard = "unnamedplus"
