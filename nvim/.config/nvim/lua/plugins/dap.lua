b183e9154607258f97caf2c13e68ff026328ac37
Lorenz <lorenz.mohr@tu-ilmenau.de>
2025-05-06 17:41:31 +0200

    dap for python working

---
+++ nvim/.config/nvim/lua/plugins/dap.lua
@@ -0,0 +1,72 @@
+return {
+    {
+        "mfussenegger/nvim-dap",
+        dependencies = {
+            "nvim-neotest/nvim-nio",
+            "rcarriga/nvim-dap-ui",
+            "mfussenegger/nvim-dap-python"
+        },
+        -- configuration
+        config = function()
+            local dap = require("dap")
+            local dapui = require("dapui")
+
+
+            -- register debugpy adapter (debugpy sits under ~/.virtualenvs/)
+            local python_path = "~/.virtualenvs/debugpy/bin/python"
+            require("dap-python").setup(python_path)
+
+            -- DAP UI
+            -- setup
+            dapui.setup()
+            -- DAP UI connection to DAP
+            dap.listeners.before.attach.dapui_config = function()
+                dapui.open()
+            end
+            dap.listeners.before.launch.dapui_config = function()
+                dapui.open()
+            end
+            dap.listeners.before.event_terminated.dapui_config = function()
+                dapui.close()
+            end
+            dap.listeners.before.event_exited.dapui_config = function()
+                dapui.close()
+            end
+
+            -- DAP Keymaps
+            vim.keymap.set('n', '<leader>dt', function()
+                dap.toggle_breakpoint()
+            end, { desc = "toggle breakpoint" })
+            vim.keymap.set('n', '<leader>dc', function()
+                dap.continue()
+            end, { desc = "continue" })
+            vim.keymap.set('n', '<leader>dso', function()
+                dap.step_over()
+            end, { desc = "step over" })
+            vim.keymap.set('n', '<leader>dsi', function()
+                dap.step_into()
+            end, { desc = "step into" })
+            vim.keymap.set('n', '<leader>dsO', function()
+                dap.step_out()
+            end, { desc = "step out" })
+            -- vim.keymap.set('n', '<Leader>lp',
+            --     function() require('dap').set_breakpoint(nil, nil, vim.fn.input('Log point message: ')) end)
+            -- vim.keymap.set('n', '<Leader>dr', function() require('dap').repl.open() end)
+            -- vim.keymap.set('n', '<Leader>dl', function() require('dap').run_last() end)
+            -- vim.keymap.set({ 'n', 'v' }, '<Leader>dh', function()
+            --     require('dap.ui.widgets').hover()
+            -- end)
+            -- vim.keymap.set({ 'n', 'v' }, '<Leader>dp', function()
+            --     require('dap.ui.widgets').preview()
+            -- end)
+            -- vim.keymap.set('n', '<Leader>df', function()
+            --     local widgets = require('dap.ui.widgets')
+            --     widgets.centered_float(widgets.frames)
+            -- end)
+            -- vim.keymap.set('n', '<Leader>ds', function()
+            --     local widgets = require('dap.ui.widgets')
+            --     widgets.centered_float(widgets.scopes)
+            -- end)
+        end,
+    }
+}
--- nvim/.config/nvim/lua/user/options.lua
+++ nvim/.config/nvim/lua/user/options.lua
@@ -1,15 +1,11 @@
 -- configuration file for user defined neovim options
 local opt = vim.opt
--- basic editor options
+-- basic options
 opt.shiftwidth = 4            -- number of spaces for an indentation
-opt.tabstop = 10              -- number of spaces for a tab char
+opt.tabstop = 4               -- number of spaces for a tab
 opt.softtabstop = 0           -- softabs
-opt.smarttab = true           -- better tabs
 opt.expandtab = true          -- convert tabs to spaces
--- system clipboard and yank buffer matching
 opt.clipboard = "unnamedplus" -- system clipboard yanking pasting etc
--- true colors
-opt.termguicolors = true
 
 -- line numbers autocommands
 -- Set relative line numbers in normal, visual, and select modes
@@ -24,6 +20,8 @@ vim.api.nvim_create_autocmd("InsertLeave", {
     pattern = "*",
     command = "set relativenumber"
 })
+-- true colors
+vim.opt.termguicolors = true
 
 -- status line
 -- set statusline=%<%f\ %h%m%r%{FugitiveStatusline()}%=%-14.(%l,%c%V%)\ %P
