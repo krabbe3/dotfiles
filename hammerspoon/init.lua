915b8bd9bbc2af2391f3eaf2276de6d7d959fc9c
Lorenz <lorenz.mohr@tu-ilmenau.de>
2025-05-08 17:07:48 +0200

    removed old shizzle

--- hammerspoon/init.lua
+++
@@ -1,7 +0,0 @@
--- Hammerspoon initialization file
-
--- Keyboard shortcuts
---open Terminal ctrl + cmd + T
-hs.hotkey.bind({"ctrl", "cmd"}, "T", function()
-    hs.application.open('Terminal.app')
-end)
