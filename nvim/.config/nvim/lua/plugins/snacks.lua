d23aaa30103fb2b9c543fe0b2c18374e9f726abe
Lorenz <lorenz.mohr@tu-ilmenau.de>
2025-07-16 15:10:37 +0200

    updated snacks

--- nvim/.config/nvim/lua/plugins/snacks.lua
+++ nvim/.config/nvim/lua/plugins/snacks.lua
@@ -5,19 +5,12 @@ return {
         lazy = false,
         ---@type snacks.Config
         opts = {
-            -- your configuration comes here
-            -- or leave it empty to use the default settings
-            -- refer to the configuration section below
             bigfile = {
                 enabled = true,
                 notify = true,
             },
             dashboard = { enabled = true },
-            image = { enabled = true },
-            explorer = { enabled = true },
             indent = { enabled = true },
-            input = { enabled = true },
-            picker = { enabled = true },
             notifier = { enabled = true },
             quickfile = { enabled = true },
             scope = { enabled = true },
